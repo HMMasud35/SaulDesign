75d82e4ba46811271f8d93a011c85297744f5c08
unknown <hmmasud635@gmail.com>
2025-04-09 20:17:33 +0600

    new add

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import Banner_img from '../assets/images/banner.png'
 import Banner_shape from '../assets/images/banner-shape.png'
-
-
 import { LuDownload } from "react-icons/lu";
+
 const Banner = () => {
   return (
-    <section className='py-50.5'>
+    <section className='py-5 sm:py-10 lg:py-50.5'>
       <div className='container'>
-        <div className='flex'>
-          <img className='w-22.5 h-58.5 mt-22 mr-5 rotate-3' src={Banner_shape} alt="Banner Shape" />
-          <div className='w-[60%]'>
-            <h1 className='text-8xl font-bold leading-24 text-font_primary mb-12'>CREATIVE UI <span className='text-secondary'>DESIGNER</span></h1>
-            <div className='flex'>
+        <div className='sm:flex'>
+          <img className='hidden lg:block w-22.5 h-58.5 mt-12 mr-5 rotate-3' src={Banner_shape} alt="Banner Shape" />
+          <div className='sm:w-[60%]'>
+            <h1 className='text-5xl lg:text-7xl font-bold leading-14 sm:leading-16 lg:leading-24 text-font_primary mb-6 lg:mb-12'>CREATIVE UI <span className='text-secondary'>DESIGNER</span></h1>
+            <div className='flex mb-2'>
               <button className='btn text_shadow mr-6'>Hire me</button>
-              <button className='btn text_shadow flex gap-3'>Download CV <LuDownload className='text-2xl' /> </button>
+              <button className='btn text_shadow flex gap-3'>Download CV <LuDownload className='text-lg lg:text-2xl' /> </button>
             </div>
           </div>
-          <div className='w-[40%]'>
-            <img className='ml-auto mt-[-20px]' src={Banner_img} alt="Banner_img" />
+          <div className='sm:w-[50%] lg:w-[40%]'>
+            <img className='ml-auto sm:mt-[-30px] lg:mt-[-20px]' src={Banner_img} alt="Banner_img" />
           </div>
         </div>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import Logo from "../../public/images/logo.png"
+import { AiOutlineBars } from "react-icons/ai";
 
 const Header = () => {
   return (
-    <header className='border-b border-font_primary/10 py-12'>
+    <header className='border-b border-font_primary/10 lg:py-12 py-3'>
       <nav>
         <div className='container'>
           <div className='flex justify-between'>
             <a href="#">
               <img src={Logo} alt="logo" />
             </a>
-            <ul className='flex gap-12'>
+            <AiOutlineBars className='text-white text-3xl font-bold lg:hidden ' />
+            <ul className='hidden lg:flex gap-12'>
               <li className='list after'><a href="#">Home</a></li>
               <li className='list after'><a href="#">About Me</a></li>
               <li className='list after'><a href="#">Contact</a></li>
